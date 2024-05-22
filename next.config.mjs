38e8dd35ef3941f1bd36e0f387c006a6f88c7c2e
ANTONIO ARTUR SANTOS MONTEIRO <ANTONIO0557@DIGITALCOLLEGE.LOCAL>
2024-05-22 20:20:20 -0300

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'cdn-icons-png.flaticon.com'
+            }
+        ]
+    }
+};
+
 
 export default nextConfig;
