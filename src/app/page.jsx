a396ce037aff5964382878cc32d969bff1916dd7
ANTONIO ARTUR SANTOS MONTEIRO <ANTONIO0557@DIGITALCOLLEGE.LOCAL>
2024-06-05 21:12:06 -0300

    integrando com api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,47 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function onSubmit() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "name": name,
+      "email": email
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("http://localhost:3001/email/register-email", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  }
+
   return (
     <main className="bg-black flex min-h-screen flex-col items-center justify-center ">
       <div className="h-[28rem] p-5 w-2/3 bg-cyan-600 rounded-lg flex">
-          <div className="relative flex justify-center items-center bg-indigo-500 flex-[2] min-h-full rounded">
-            <Image src={"https://cdn-icons-png.flaticon.com/512/4939/4939994.png"} alt="hero" layout="fill" objectFit="contain" className="rounded max-h-36 flex mt-20" />
-          </div>
-          <div className="flex-[3] px-16 min-h-full flex flex-col text-white justify-center items-center gap-5">
-            <h1 className="text-2xl">Se inscreva para receber as novidades!</h1>
-            <p className="text-lg">Nunca mais se esqueça de comprar seus insumos semanais com qualidade de vida!</p>
-            <input className="w-full p-2 rounded text-black" placeholder="Digite seu nome" type="text" />
-            <input className="w-full p-2 rounded text-black" placeholder="Digite seu email" type="email" />
-            <button className="bg-indigo-500 p-4 rounded min-w-full">Inscreva-se</button>
-          </div>
+        <div className="relative flex justify-center items-center bg-indigo-500 flex-[2] min-h-full rounded">
+          <Image src={"https://cdn-icons-png.flaticon.com/512/4939/4939994.png"} alt="hero" layout="fill" objectFit="contain" className="rounded max-h-36 flex mt-20" />
+        </div>
+        <div className="flex-[3] px-16 min-h-full flex flex-col text-white justify-center items-center gap-5">
+          <h1 className="text-2xl">Se inscreva para receber as novidades!</h1>
+          <p className="text-lg">Nunca mais se esqueça de comprar seus insumos semanais com qualidade de vida!</p>
+          <input value={name} onChange={e => setName(e.target.value)} className="w-full p-2 rounded text-black" placeholder="Digite seu nome" type="text" />
+          <input value={email} onChange={e => setEmail(e.target.value)} className="w-full p-2 rounded text-black" placeholder="Digite seu email" type="email" />
+          <button onClick={onSubmit} className="bg-indigo-500 p-4 rounded min-w-full">Inscreva-se</button>
+        </div>
       </div>
     </main>
   );
